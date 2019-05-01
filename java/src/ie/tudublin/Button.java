60a6b5bdc8107a764d250f16bdb109cc24979eff
Fionn MacNamara <fionnmacn@gmail.com>
2019-05-01 08:59:01 +0100

    Added Button class

---
+++ java/src/ie/tudublin/Button.java
@@ -0,0 +1,125 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Button
+{
+    UI ui;
+    private float x;
+    private float y;
+    private float width;
+    private float height;
+    private String text;
+
+    public Button(UI ui, float x, float y, float width, float height, String text)
+    {
+        this.ui = ui;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.text = text;
+    }
+
+    public void render()
+    {
+        ui.fill(70);
+        ui.stroke(255);
+        ui.strokeWeight(2);
+        ui.rect(x, y, width, height);
+        ui.textAlign(PApplet.CENTER, PApplet.CENTER);
+        ui.fill(255);
+        ui.textSize(25);
+        ui.text(text, x + width * 0.5f, y + height * 0.5f);
+    }
+
+    /**
+     * @return the ui
+     */
+    public UI getUi() {
+        return ui;
+    }
+
+    /**
+     * @param ui the ui to set
+     */
+    public void setUi(UI ui) {
+        this.ui = ui;
+    }
+
+    /**
+     * @return the x
+     */
+    public float getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the width
+     */
+    public float getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    /**
+     * @return the height
+     */
+    public float getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    //toString Method
+    @Override
+    public String toString() {
+        return "Button [height=" + height + ", text=" + text + ", ui=" + ui + ", width=" + width + ", x=" + x + ", y=" + y + "]";
+    }
+}
